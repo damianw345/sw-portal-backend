a291c317557053ae6ee3e92c7db9d2c11b454eeb
damianw345 <damianw345@gmail.com>
2019-10-13 23:35:28 +0200

    - Add CORS support for ResourceController - Add separate property file for Docker to have environment variables loaded after build
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/main/kotlin/com/github/damianw345/swportalbackend/rest/controller/ResourceController.kt
+++ src/main/kotlin/com/github/damianw345/swportalbackend/rest/controller/ResourceController.kt
@@ -5,13 +5,11 @@ import org.bson.Document
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = ["\${frontend.url}"])
 class ResourceController constructor(val resourceService: ResourceService) {
 
     @GetMapping("/{resourceType}/{id}")
