673e9c347b8956d298285837ba158b3ddaccb62a
damianw345 <damianw345@gmail.com>
2019-10-13 12:39:52 +0200

    - Fix context integration test failing because of try to connect Mongo in non dockerized environment during the build - Preventing CommandLineRunner to run during tests
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/test/kotlin/com/github/damianw345/swportalbackend/SwPortalBackendApplicationTests.kt
+++ src/test/kotlin/com/github/damianw345/swportalbackend/SwPortalBackendApplicationTests.kt
@@ -1,16 +1,26 @@
-//package com.github.damianw345.swportalbackend
-//
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//import org.springframework.boot.test.context.SpringBootTest
-//import org.springframework.test.context.junit4.SpringRunner
-//
-//@RunWith(SpringRunner::class)
-//@SpringBootTest
-//class SwPortalBackendApplicationTests {
-//
-//    @Test
-//    fun contextLoads() {
-//    }
-//
-//}
+package com.github.damianw345.swportalbackend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = [MongoAutoConfiguration::class, MongoDataAutoConfiguration::class])
+@ActiveProfiles("test")
+class SwPortalBackendApplicationTests {
+
+    @MockBean
+    lateinit var mongoTemplate: MongoTemplate
+
+    @Test
+    fun contextLoads() {
+    }
+}
