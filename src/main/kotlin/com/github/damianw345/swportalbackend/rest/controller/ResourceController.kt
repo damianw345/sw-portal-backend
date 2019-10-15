113738286093e893c1728c08dbcb3ddd4c820617
damianw345 <damianw345@gmail.com>
2019-10-15 22:20:07 +0200

    Add HATEOAS style _links section to paginated resources
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/main/kotlin/com/github/damianw345/swportalbackend/rest/controller/ResourceController.kt
+++ src/main/kotlin/com/github/damianw345/swportalbackend/rest/controller/ResourceController.kt
@@ -2,9 +2,14 @@ package com.github.damianw345.swportalbackend.rest.controller
 
 import com.github.damianw345.swportalbackend.service.ResourceService
 import org.bson.Document
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
+import org.springframework.data.web.PagedResourcesAssembler
+import org.springframework.hateoas.PagedResources
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -22,8 +27,16 @@ class ResourceController constructor(val resourceService: ResourceService) {
     @GetMapping("/{resourceType}")
     fun getResourcePage(
             @PathVariable("resourceType") resourceType: String,
-            @PageableDefault(size = 10, page = 0) pageable: Pageable
-    ): Page<Document> {
-        return resourceService.getPagedResources(pageable, resourceType)
+            @PageableDefault(size = 10, page = 0) pageable: Pageable,
+            pagedResourcesAssembler: PagedResourcesAssembler<Document>
+    ): ResponseEntity<PagedResources<Resource<Document>>> {
+
+        val link = linkTo(methodOn(ResourceController::class.java)
+                .getResourcePage(resourceType, pageable, pagedResourcesAssembler))
+                .withSelfRel()
+        val pagedResources = resourceService.getPagedResources(pageable, resourceType)
+        return ResponseEntity
+                .ok()
+                .body(pagedResourcesAssembler.toResource(pagedResources, link))
     }
 }
