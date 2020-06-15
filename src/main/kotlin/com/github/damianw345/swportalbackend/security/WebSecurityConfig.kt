071efc05a7cd83ed2c62c4b0673ef8d4bdeab56e
damianw345 <damianw345@gmail.com>
2020-06-15 23:35:30 +0200

    Remove authentication from Swagger documentation endpoint
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/main/kotlin/com/github/damianw345/swportalbackend/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/github/damianw345/swportalbackend/security/WebSecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -49,6 +50,16 @@ class WebSecurityConfig(
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+    }
+
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**"
+        )
     }
 
     override fun userDetailsServiceBean(): UserDetailsService {
